4887a5118d1d1e5ee3892c409695c2dcf9c8f5ca
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-05-12 21:30:07 +0600

    final Deploy

--- src/app/lib/auth-client.js
+++ src/app/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://b13-a8-tiles-gallery-3p1e.vercel.app"
+    baseURL: "https://b13-a8-tiles-gallery-rq3g.vercel.app"
 })
 export const { signIn, signUp, signOut, useSession } = createAuthClient()
