99f016588e0f786160474ee4801fcf3ba9bee786
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-05-13 00:05:29 +0600

    add url

--- src/app/lib/auth-client.js
+++ src/app/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://b13-a8-tiles-gallery-rq3g.vercel.app"
 })
 export const { signIn, signUp, signOut, useSession } = createAuthClient()
